3702f0c07c6b22cfd4bc2c6c5979a144f842a2ef
Konstantin Tsabolov <1006564+tsabolov@users.noreply.github.com>
2024-01-14 13:45:10 +0100

    chore(CI): setup GitHub Actions

---
+++ e2e/jest.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "..",
+  testEnvironment: "node",
+  testRegex: ".e2e-spec.ts$",
+  verbose: true,
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageReporters:
+    process.env.CI === "true" || process.env.CI === true
+      ? ["text-summary", "cobertura"]
+      : ["lcov"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/e2e/",
+    ".dto.(t|j)s",
+    ".interface.(t|j)s",
+  ],
+  coverageDirectory: "./e2e-coverage",
+  coverageProvider: "v8",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+};
